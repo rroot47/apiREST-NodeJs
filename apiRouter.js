e6aaab028640c6764e5c2a49363df610f6c38c25
unknown <boubousylla2@gmail.com>
2020-12-20 03:24:07 +0100

    add swagger api

--- apiRouter.js
+++ apiRouter.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var userController = require('./routes/usersController');
-
+var swaggerUi = require('swagger-ui-express');
+// var swaggerJsDoc = require('swagger-jsdoc');
+var swaggerDocument = require('./swagger.json');
 // Router
 exports.router = ((req, res) => {
     var apiRouter = express.Router();
 
     //Users routes
-    apiRouter.route('/users/register/').post(userController.register);
-    apiRouter.route('/users/login/').post(userController.login);
+    apiRouter.use('/api/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    apiRouter.route('/users/register').post(userController.register);
+    apiRouter.route('/users/login').post(userController.login);
     return apiRouter;
 })();
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser');
 var apiRouter = require('./apiRouter').router
 var swaggerUi = require('swagger-ui-express');
 var swaggerJsDoc = require('swagger-jsdoc');
-const swaggerDocument = require('./swagger.json');
+var swaggerDocument = require('./swagger.json');
+
 // Instantion de server
 var server = express();
 
@@ -12,16 +13,8 @@ var server = express();
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-var swaggerDocs = swaggerJsDoc(swaggerDocument);
-server.use('api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-// configuration desroutes
-server.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Bonjour Toute le monde</h1>')
-});
-
 // assigner le route
-server.use('/api/', apiRouter);
+server.use(apiRouter);
 //launcher server
 server.listen(3000, () => {
     console.log('server running in port 3000');
